7a453ca1471a9cc5806b5dd9f4fbb7287a653dda
Pocketo <jsmith14827@gmail.com>
2025-10-15 18:49:09 -0600

    Arma hace daño, enemigos reciben daño y puede moriri

--- Assets/Scripts/EnemuHealth.cs
+++ Assets/Scripts/EnemuHealth.cs
@@ -5,20 +5,23 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] private int health;
     
     private int maxHealth;
-    
+    private Animator ani;
     void Start()
     {
         maxHealth = health;
+        ani = GetComponent<Animator>();
     }
 
     public void TakeDamage(int damage)
     {
+        ani.SetTrigger("Hit");
         health -= damage;
         Debug.Log(gameObject.name + " recibió " + damage + " de daño. Vida: " + health);
         
         if (health <= 0)
         {
-            Destroy(gameObject);
+            ani.SetTrigger("Death");
+            Destroy(gameObject, 3.0f);
         }
     }
 
